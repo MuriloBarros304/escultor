5ab00bb821c2526afa4f55f38fee379f9c9c7e04
Murilo Barros <murilobarros304@gmail.com>
2024-04-30 14:04:51 -0300

    putsphere(verificar)

--- sculptor.cpp
+++ sculptor.cpp
@@ -72,14 +72,14 @@ void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
   }
 }
 
-void putSphere(int xcenter, int ycenter, int zcenter, int radius){
+void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
   int i, j, k;
   // (x - a)² + (y - b)² + (z - c)² = r²
   for(i=(-xcenter);i<=xcenter;i++){ // (x - a)² = r² -> x² - 2xa + a² = r² -> x² = 2 * x * a - a * a
     for(j=(-ycenter);j<=ycenter;j++){ // (y - b)² = r²
       for(k=(-zcenter);k<=zcenter;k++){ // (z - c)² = r²
-        if(i*i == (2*i*xcenter - xcenter*xcenter)){
-          v[i][][].show = true;
+        if(i*i == (2*i*xcenter - xcenter*xcenter) and j*j == (2*j*ycenter - ycenter*ycenter) and k*k == (2*k*zcenter - zcenter*zcenter)){
+          v[i][j][k].show = true;
           v[i][j][k].r = r;
           v[i][j][k].g = g;
           v[i][j][k].b = b;
