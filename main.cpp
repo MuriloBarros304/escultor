cc8f9bf7f2b13b01d34fb47518a777f5c8b6114e
Murilo Barros <murilobarros304@gmail.com>
2024-05-01 17:50:27 -0300

    testes

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include "sculptor.h"
 
 int main(void){
-  Sculptor teste(4, 3, 2);
-  teste.setColor(0.5, 0.6, 0.0, 1);
+  Sculptor teste(100, 100, 100);
+  teste.setColor(1, 0, 0.0, 1);
   teste.putVoxel(1, 1, 1);
-  teste.putEllipsoid(3, 3, 4, 2, 1, 5);
+  teste.putEllipsoid(25, 50, 50, 10, 20, 25);
+  teste.setColor(0,1,0,0.9);
+  teste.putSphere(50,50,50,40);
+  teste.writeOFF("teste.off");
 }
--- sculptor.cpp
+++ sculptor.cpp
@@ -83,17 +83,17 @@ void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
   z0 = zcenter-radius;
   z1 = zcenter+radius;
   for(i=(x0); i<=(x1); i++){
-    dx = (i*xcenter)*(i-xcenter); 
+    dx = (i-xcenter)*(i-xcenter);   
       for(j=(y0); j<=(y1); j++){
-        dy = (j*ycenter)*(j-ycenter);
-          for(k=(z0); k<=(z1); k++){ 
-            dz = (k*zcenter)*(k-zcenter);
-        if(dx + dy + dz == 1){
-          v[i][j][k].show = true;
-          v[i][j][k].r = r;
-          v[i][j][k].g = g;
-          v[i][j][k].b = b;
-          v[i][j][k].a = a;
+        dy = (j-ycenter)*(j-ycenter);
+          for(k=(z0); k<=(z1); k++){
+            dz = (k-zcenter)*(k-zcenter);
+            if(dx + dy + dz <= (radius*radius)){
+              v[i][j][k].show = true;
+              v[i][j][k].r = r;
+              v[i][j][k].g = g;
+              v[i][j][k].b = b;
+              v[i][j][k].a = a;
         }
       }
     }
@@ -102,6 +102,8 @@ void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
 
 void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
   int i, j, k, x0, x1, y0, y1, z0, z1, dx, dy, dz;
+   // (x - a)² + (y - b)² + (z - c)² = r²
+   // limites da esfera
   x0 = xcenter-radius; 
   x1 = xcenter+radius;
   y0 = ycenter-radius;
@@ -109,13 +111,13 @@ void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
   z0 = zcenter-radius;
   z1 = zcenter+radius;
   for(i=(x0); i<=(x1); i++){
-    dx = (i*xcenter)*(i-xcenter); 
+    dx = (i-xcenter)*(i-xcenter);   
       for(j=(y0); j<=(y1); j++){
-        dy = (j*ycenter)*(j-ycenter);
-          for(k=(z0); k<=(z1); k++){ 
-            dz = (k*zcenter)*(k-zcenter);
-        if(dx + dy + dz == 1){
-          v[i][j][k].show = false;
+        dy = (j-ycenter)*(j-ycenter);
+          for(k=(z0); k<=(z1); k++){
+            dz = (k-zcenter)*(k-zcenter);
+            if(dx + dy + dz <= (radius*radius)){
+              v[i][j][k].show = false;
         }
       }
     }
@@ -132,18 +134,18 @@ void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
   y1 = ycenter+ry;
   z0 = zcenter-rz;
   z1 = zcenter+rz;
-  for(i=(x0); i<=(x1); i++){
-    dx = (i*xcenter)*(i-xcenter)/(rx*rx); // evitar repetições
-      for(j=(y0); j<=(y1); j++){
-        dy = (j*ycenter)*(j-ycenter)/(ry*ry);
-          for(k=(z0); k<=(z1); k++){ 
-            dz = (k*zcenter)*(k-zcenter)/(rz*rz);
-        if(dx + dy + dz == 1){
-          v[i][j][k].show = true;
-          v[i][j][k].r = r;
-          v[i][j][k].g = g;
-          v[i][j][k].b = b;
-          v[i][j][k].a = a;
+  for(i=x0; i<=x1; i++){
+    dx = ((i-xcenter)*(i-xcenter))/(rx*rx); 
+      for(j=y0; j<=y1; j++){
+        dy = ((j-ycenter)*(j-ycenter))/(ry*ry);
+          for(k=z0; k<=z1; k++){ 
+            dz = ((k-zcenter)*(k-zcenter))/(rz*rz);
+            if((dx + dy + dz) <= 1){
+              v[i][j][k].show = true;
+              v[i][j][k].r = r;
+              v[i][j][k].g = g;
+              v[i][j][k].b = b;
+              v[i][j][k].a = a;
         }
       }
     }
@@ -161,15 +163,113 @@ void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
   z0 = zcenter-rz;
   z1 = zcenter+rz;
   for(i=(x0); i<=(x1); i++){
-    dx = (i*xcenter)*(i-xcenter)/(rx*rx); // evitar repetições
+    dx = (i-xcenter)*(i-xcenter)/(rx*rx); // evitar repetições
       for(j=(y0); j<=(y1); j++){
-        dy = (j*ycenter)*(j-ycenter)/(ry*ry);
+        dy = (j-ycenter)*(j-ycenter)/(ry*ry);
           for(k=(z0); k<=(z1); k++){ 
-            dz = (k*zcenter)*(k-zcenter)/(rz*rz);
-        if(dx + dy + dz == 1){
-          v[i][j][k].show = false;
+            dz = (k-zcenter)*(k-zcenter)/(rz*rz);
+              if(dx + dy + dz <= 1){
+                v[i][j][k].show = false;
         }
       }
     }
   }
+}
+
+// a partir daqui o código foi tirado de outros alunos, para testar as funções acima
+void Sculptor::writeOFF(const char *filename)
+{
+    std::ofstream fout;
+    int nVoxel = 0, ref = 0;
+    fout.open(filename);
+
+    if (fout.is_open())
+    {
+        std::cout << "Gravando o arquivo...\n";
+    }
+    else
+    {
+        std::cout << "Erro na abertura do arquivo\n";
+        std::cout << "Pressione Enter para fechar a janela.\n";
+        std::cin.get();
+        exit(1); // Encerrando o programa
+    }
+
+    for (int i = 0; i < nz; i++)
+    {
+        for (int j = 0; j < nx; j++)
+        {
+            for (int k = 0; k < ny; k++)
+            {
+                if (v[i][j][k].show)
+                {
+                    nVoxel++;
+                }
+            }
+        }
+    }
+
+    fout << "OFF" << std::endl;
+    fout << nVoxel * 8 << " " << nVoxel * 6 << " " << 0 << std::endl;
+
+    for (int i = 0; i < nz; i++)
+    {
+        for (int j = 0; j < nx; j++)
+        {
+            for (int k = 0; k < ny; k++)
+            {
+                if (v[i][j][k].show)
+                {
+                    fout << i - 0.5 << " " << j + 0.5 << " " << k - 0.5 << std::endl;
+                    fout << i - 0.5 << " " << j - 0.5 << " " << k - 0.5 << std::endl;
+                    fout << i + 0.5 << " " << j - 0.5 << " " << k - 0.5 << std::endl;
+                    fout << i + 0.5 << " " << j + 0.5 << " " << k - 0.5 << std::endl;
+                    fout << i - 0.5 << " " << j + 0.5 << " " << k + 0.5 << std::endl;
+                    fout << i - 0.5 << " " << j - 0.5 << " " << k + 0.5 << std::endl;
+                    fout << i + 0.5 << " " << j - 0.5 << " " << k + 0.5 << std::endl;
+                    fout << i + 0.5 << " " << j + 0.5 << " " << k + 0.5 << std::endl;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < nz; i++)
+    {
+        for (int j = 0; j < nx; j++)
+        {
+            for (int k = 0; k < ny; k++)
+            {
+                if (v[i][j][k].show)
+                {
+                    fout << 4 << " " << ref + 0 << " " << ref + 3 << " " << ref + 2 << " " << ref + 1 << " "
+                         << v[i][j][k].r << " " << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << std::endl;
+
+                    fout << 4 << " " << ref + 4 << " " << ref + 5 << " " << ref + 6 << " " << ref + 7 << " "
+                         << v[i][j][k].r << " " << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << std::endl;
+
+                    fout << 4 << " " << ref + 0 << " " << ref + 1 << " " << ref + 5 << " " << ref + 4 << " "
+                         << v[i][j][k].r << " " << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << std::endl;
+
+                    fout << 4 << " " << ref + 0 << " " << ref + 4 << " " << ref + 7 << " " << ref + 3 << " "
+                         << v[i][j][k].r << " " << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << std::endl;
+
+                    fout << 4 << " " << ref + 3 << " " << ref + 7 << " " << ref + 6 << " " << ref + 2 << " "
+                         << v[i][j][k].r << " " << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << std::endl;
+
+                    fout << 4 << " " << ref + 1 << " " << ref + 2 << " " << ref + 6 << " " << ref + 5 << " "
+                         << v[i][j][k].r << " " << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << std::endl;
+                         
+                    ref += 8;
+                }
+            }
+        }
+    }
+
+    if (fout.is_open())
+    {
+        std::cout << "Arquivo gerado com sucesso!\n";
+        std::cout << "Pressione Enter para fechar a janela.\n";
+        std::cin.get();
+        exit(1); // Encerrando o programa
+    }
 }
