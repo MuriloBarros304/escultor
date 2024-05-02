34b21d484459474c9c54c4b10ee0379202582315
Murilo Barros <murilobarros304@gmail.com>
2024-05-02 09:22:24 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(void){
   teste.setColor(1, 0, 0.0, 1);
   teste.putVoxel(1, 1, 1);
   teste.putEllipsoid(25, 50, 50, 10, 20, 25);
-  teste.setColor(0,1,0,0.9);
-  teste.putSphere(50,50,50,40);
+  //teste.setColor(0,1,0,0.9);
+  //teste.putSphere(50,50,50,40);
   teste.writeOFF("teste.off");
 }
