cc940a07e0610b71f5e8cb1f778cf38853bfceb5
Murilo Barros <murilobarros304@gmail.com>
2024-05-05 14:33:54 -0300

    update

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 
 int main(void){
   Sculptor teste(20, 20, 20);
-  teste.setColor(0.9, 0.5, 0.0, 0.9);
+  teste.setColor(0.9, 0.5, 0.1, 0.9);
   teste.putVoxel(0,0,0);
-  //teste.setColor(0.9, 0.1, 0.1, 0.9);
+  teste.setColor(0.9, 0.1, 0.1, 0.9);
   teste.putBox(1,5,1,6,1,8);
+  teste.putSphere(10, 10, 10, 3);
   //teste.putVoxel(1, 1, 1);
   //teste.putVoxel(5, 5, 5);
   //teste.putEllipsoid(25, 50, 50, 10, 20, 25);
