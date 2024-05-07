b9489d614a368ab94979f7c32cc8daf56a44c3f2
Murilo Barros <murilobarros304@gmail.com>
2024-05-07 12:11:10 -0300

    inicio de um desenho

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include "sculptor.h"
 
 int main(void){
-  Sculptor teste(21, 21, 21);
-  teste.setColor(0.9, 0.5, 0.1, 0.9);
-  teste.putVoxel(0,0,0);
-  teste.setColor(0.9, 0.1, 0.1, 0.9);
-  //teste.putBox(1,19,1,19,1,19);
-  teste.putVoxel(20, 20, 20);
-  teste.setColor(0.1,0.1,0.9, 0.9);
-  teste.putEllipsoid(10,10,10,5,6,9);
-  teste.setColor(0.5,0.9,0.5,0.9);
-  teste.cutBox(10, 12, 1, 19, 1, 19);
-  //teste.putVoxel(1, 1, 1);
-  //teste.putVoxel(5, 5, 5);
-  //teste.putEllipsoid(25, 50, 50, 10, 20, 25);
-  //teste.setColor(0,1,0,0.9);
-  //teste.putSphere(50,50,50,40);
-  teste.writeOFF("teste.off");
+  Sculptor quarto(100, 100, 100);
+  quarto.setColor(0.9, 0.9, 0.9, 0.9);
+  quarto.putBox(0, 99, 0, 99, 0, 99);
+  quarto.cutBox(2, 97, 2, 99, 2, 99);
+  quarto.setColor(0.99, 0.99, 0.10, 0.9);
+  quarto.putBox(2, 12, 42, 58, 48, 64);
+  quarto.cutBox(3, 13, 43, 57, 49, 63);
+  quarto.putBox(2, 12, 58, 74, 64, 80);
+  quarto.cutBox(3, 13, 59, 73, 65, 79);
+  quarto.putBox(2, 12, 26, 42, 32, 48);
+  quarto.cutBox(3, 13, 27, 41, 33, 47);
+  quarto.setColor(0.39, 0.18, 0.01, 0.9);
+  quarto.putBox(34, 68, 34, 68, 0, 3);
+  quarto.cutBox(36, 50, 36, 50, 0, 3);
+  quarto.cutBox(36, 50, 52, 66, 0, 3);
+  quarto.cutBox(52, 66, 52, 66, 0, 3);
+  quarto.cutBox(52, 66, 36, 50, 0, 3);
+
+  quarto.writeOFF("quarto.off");
 }
