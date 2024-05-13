fccc796dda43afdee43cbd40d9262be13672fba3
Murilo Barros <murilobarros304@gmail.com>
2024-05-13 18:58:54 -0300

    teste doxygen

---
+++ html/search/all_7.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['sculptor_16',['Sculptor',['../class_sculptor.html',1,'Sculptor'],['../class_sculptor.html#a014e3ef5517bf0e9d9e14486b6ac6433',1,'Sculptor::Sculptor()']]],
+  ['sculptor_2ecpp_17',['sculptor.cpp',['../sculptor_8cpp.html',1,'']]],
+  ['sculptor_2eh_18',['sculptor.h',['../sculptor_8h.html',1,'']]],
+  ['setcolor_19',['setColor',['../class_sculptor.html#a5723e671e073ac310b6f385a7fdc9f9f',1,'Sculptor']]],
+  ['show_20',['show',['../struct_voxel.html#a7daa6a3073416618d1365e0fc38f152f',1,'Voxel']]]
+];
