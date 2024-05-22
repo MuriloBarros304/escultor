6b91450ced554cceb097b43f2327b52089ab8225
Murilo Barros <murilobarros304@gmail.com>
2024-05-22 12:45:16 -0300

    update

--- sculptor.h
+++ sculptor.h
@@ -17,10 +17,8 @@ class Sculptor {
     void cutBox(int x0, int x1, int y0, int y1, int z0, int z1);
     void putSphere(int xcenter, int ycenter, int zcenter, int radius);
     void cutSphere(int xcenter, int ycenter, int zcenter, int radius);
-    void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry,
-                      int rz);
-    void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry,
-                      int rz);
+    void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
+    void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
     void writeOFF(const char* filename);
 };
 
