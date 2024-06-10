eef00cd68ba2453bc72a84e4a3ba8a75f9edf648
Murilo Barros <murilobarros304@gmail.com>
2024-06-10 13:36:25 -0300

    correcao de detalhes

--- readtxt.cpp
+++ readtxt.cpp
@@ -27,67 +27,65 @@ ReadTXT::ReadTXT(const char* filename) {
     }
     while (fin.good()) {
         fin >> s;
-        if (fin.good()) {
-            if (s.compare("dim") == 0) {
-                int x, y, z;
-                fin >> x >> y >> z;
-                nx = x;
-                ny = y;
-                nz = z;
-                std::cout << "Dimensões lidas: " << nx << ", " << ny << ", "
-                          << nz << "\n";
-            } else if (s.compare("putvoxel") == 0) {
-                int x0, y0, z0;
-                float r, g, b, a;
-                fin >> x0 >> y0 >> z0;
-                fin >> r >> g >> b >> a;
-                vetor.push_back(new PutVoxel(x0, y0, z0, r, g, b, a));
-            } else if (s.compare("cutvoxel") == 0) {
-                int x0, y0, z0;
-                fin >> x0 >> y0 >> z0;
-                vetor.push_back(new CutVoxel(x0, y0, z0));
-            } else if (s.compare("putbox") == 0) {
-                int x0, y0, z0, x1, y1, z1;
-                float r, g, b, a;
-                fin >> x0 >> x1 >> y0;
-                fin >> y1 >> z0 >> z1;
-                fin >> r >> g >> b >> a;
-                vetor.push_back(new PutBox(x0, x1, y0, y1, z0, z1, r, g, b, a));
-            } else if (s.compare("cutbox") == 0) {
-                int x0, y0, z0, x1, y1, z1;
-                float r, g, b, a;
-                fin >> x0 >> x1 >> y0;
-                fin >> y1 >> z0 >> z1;
-                vetor.push_back(new CutBox(x0, x1, y0, y1, z0, z1));
-            } else if (s.compare("putsphere") == 0) {
-                int xcenter, ycenter, zcenter, radius;
-                float r, g, b, a;
-                fin >> xcenter >> ycenter >> zcenter >> radius;
-                fin >> r >> g >> b >> a;
-                vetor.push_back(new PutSphere(xcenter, ycenter, zcenter, radius,
-                                              r, g, b, a));
-            } else if (s.compare("cutsphere") == 0) {
-                int xcenter, ycenter, zcenter, radius;
-                float r, g, b, a;
-                fin >> xcenter >> ycenter >> zcenter >> radius;
-                vetor.push_back(
-                    new CutSphere(xcenter, ycenter, zcenter, radius));
-            } else if (s.compare("putellipsoid") == 0) {
-                int xcenter, ycenter, zcenter, rx, ry, rz;
-                float r, g, b, a;
-                fin >> xcenter >> ycenter >> zcenter;
-                fin >> rx >> ry >> rz;
-                fin >> r >> g >> b >> a;
-                vetor.push_back(new PutEllipsoid(xcenter, ycenter, zcenter, rx,
-                                                 ry, rz, r, g, b, a));
-            } else if (s.compare("cutellipsoid") == 0) {
-                int xcenter, ycenter, zcenter, rx, ry, rz;
-                float r, g, b, a;
-                fin >> xcenter >> ycenter >> zcenter;
-                fin >> rx >> ry >> rz;
-                vetor.push_back(
-                    new CutEllipsoid(xcenter, ycenter, zcenter, rx, ry, rz));
-            }
+        if (s.compare("dim") == 0) {
+            int x, y, z;
+            fin >> x >> y >> z;
+            nx = x;
+            ny = y;
+            nz = z;
+            std::cout << "Dimensões lidas: " << nx << ", " << ny << ", "
+                        << nz << "\n";
+        } else if (s.compare("putvoxel") == 0) {
+            int x0, y0, z0;
+            float r, g, b, a;
+            fin >> x0 >> y0 >> z0;
+            fin >> r >> g >> b >> a;
+            vetor.push_back(new PutVoxel(x0, y0, z0, r, g, b, a));
+        } else if (s.compare("cutvoxel") == 0) {
+            int x0, y0, z0;
+            fin >> x0 >> y0 >> z0;
+            vetor.push_back(new CutVoxel(x0, y0, z0));
+        } else if (s.compare("putbox") == 0) {
+            int x0, y0, z0, x1, y1, z1;
+            float r, g, b, a;
+            fin >> x0 >> x1 >> y0;
+            fin >> y1 >> z0 >> z1;
+            fin >> r >> g >> b >> a;
+            vetor.push_back(new PutBox(x0, x1, y0, y1, z0, z1, r, g, b, a));
+        } else if (s.compare("cutbox") == 0) {
+            int x0, y0, z0, x1, y1, z1;
+            float r, g, b, a;
+            fin >> x0 >> x1 >> y0;
+            fin >> y1 >> z0 >> z1;
+            vetor.push_back(new CutBox(x0, x1, y0, y1, z0, z1));
+        } else if (s.compare("putsphere") == 0) {
+            int xcenter, ycenter, zcenter, radius;
+            float r, g, b, a;
+            fin >> xcenter >> ycenter >> zcenter >> radius;
+            fin >> r >> g >> b >> a;
+            vetor.push_back(new PutSphere(xcenter, ycenter, zcenter, radius,
+                                            r, g, b, a));
+        } else if (s.compare("cutsphere") == 0) {
+            int xcenter, ycenter, zcenter, radius;
+            float r, g, b, a;
+            fin >> xcenter >> ycenter >> zcenter >> radius;
+            vetor.push_back(
+                new CutSphere(xcenter, ycenter, zcenter, radius));
+        } else if (s.compare("putellipsoid") == 0) {
+            int xcenter, ycenter, zcenter, rx, ry, rz;
+            float r, g, b, a;
+            fin >> xcenter >> ycenter >> zcenter;
+            fin >> rx >> ry >> rz;
+            fin >> r >> g >> b >> a;
+            vetor.push_back(new PutEllipsoid(xcenter, ycenter, zcenter, rx,
+                                                ry, rz, r, g, b, a));
+        } else if (s.compare("cutellipsoid") == 0) {
+            int xcenter, ycenter, zcenter, rx, ry, rz;
+            float r, g, b, a;
+            fin >> xcenter >> ycenter >> zcenter;
+            fin >> rx >> ry >> rz;
+            vetor.push_back(
+                new CutEllipsoid(xcenter, ycenter, zcenter, rx, ry, rz));
         }
     }
     std::cout << "Arquivo de entrada lido\n";
